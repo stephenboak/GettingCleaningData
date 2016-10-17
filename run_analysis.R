9cafda120b4a96edf3175f68887b3692ddda43de
Stephen Boak <stephenboak@mac.com>
2016-10-17 17:45:16 +0100

    Initial setup of code structure

---
+++ run_analysis.R
@@ -0,0 +1,34 @@
+## 1. Merges the 'training' and the 'test' data sets to create one data set.
+    ## 1.a. 
+    ## Column-bind activity (y-train/test.txt data), subject
+    ## (subject_train/test.txt data) and measurements (X_train/test.txt data).
+
+
+
+
+    ## 1.b.
+    ## Row-bind resulting test and train data.
+
+
+
+
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+## This has been restricted to 66 measurements that form mean and standard deviation pairs.
+
+
+
+## 3. Use descriptive activity names to name the 6 activities
+## (e.g. 1:WALKING, 2:WALKING_UPSTAIRS, etc ) in the data set.
+
+
+
+## 4. Appropriately label the data set with descriptive variable names
+## (e.g. tBodyAccMeanX, tBodyAccStdX).
+
+
+
+## 5. From the data set in step 4, creates a second, independent tidy data set
+## with the average of each variable for each activity and each subject.
+## The data has been arranged to give 'activity' primacy as is suggested with
+## the wording in this step.
+
